7260d132d04cbabd029347af87b85b2feeb8e55e
lsnginterests <lsnginterests@gmail.com>
2026-07-13 21:14:51 +0300

    add pipeline runner with channels config

---
+++ extract_load/run.py
@@ -0,0 +1,18 @@
+import yaml
+from pathlib import Path
+from extract_load.extract.fetch_youtube_data import YoutubeClient
+from extract_load.extract.extractor import Extractor
+from extract_load.load.loader import LocalRawSink
+
+_CHANNELS_CONFIG = Path(__file__).resolve().parent.joinpath('extract/channels.yml')
+
+if __name__ == '__main__':
+    channel_ids = []
+    with open(_CHANNELS_CONFIG) as file:
+        for ch in yaml.safe_load(file)['channels']:
+            channel_ids.append((ch['id']))
+    extractor = Extractor(YoutubeClient())
+    loader = LocalRawSink()
+    for ch in channel_ids:
+        data = extractor.extract_channel_data(ch)
+        loader.write_raw(data)
