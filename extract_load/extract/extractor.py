46ab4e6114d6806ae056d4a878ad414d26110dab
lsnginterests <lsnginterests@gmail.com>
2026-07-13 21:14:51 +0300

    add channel extractor

---
+++ extract_load/extract/extractor.py
@@ -0,0 +1,16 @@
+from . import YoutubeClient
+
+class Extractor:
+    def __init__(self, client: YoutubeClient):
+        self.client = client
+
+    def extract_channel_data(self, channel_id):
+        channel = self.client.get_channel_data(channel_id)
+        playlist_id = self.client.get_playlist_id(channel)
+        video_ids = self.client.get_video_ids(playlist_id)
+        video = self.client.get_video_data(video_ids)
+        return {
+            'channel_id': channel_id,
+            'channel_data': channel,
+            'video_data': video
+        }
