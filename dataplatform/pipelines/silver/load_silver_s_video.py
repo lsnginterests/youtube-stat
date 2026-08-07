8d63a32b73c7142a8d01558f4bbe344afdffb60f
lsnginterests <lsnginterests@gmail.com>
2026-08-07 16:42:39 +0300

    type video attributes in silver satellite

--- dataplatform/pipelines/silver/load_silver_s_video.py
+++ dataplatform/pipelines/silver/load_silver_s_video.py
@@ -19,13 +19,13 @@ spark.sql('''
         title string,
         description string,
         category_id string,
-        is_stream string,
+        live_broadcast_content string,
         default_language string,
-        duration string,
+        duration_sec int,
         dimension string,
         definition string,
-        caption string,
-        license string,
+        caption boolean,
+        license boolean,
         content_rating string,
         projection string,
         valid_from_dttm timestamp,
@@ -35,19 +35,28 @@ spark.sql('''
     partitioned by (days(valid_from_dttm))
 ''')
 
+DURATION_PATTERN = r'^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$'
+
+duration_sec = f.when(
+    f.col('duration').rlike(DURATION_PATTERN),
+    f.coalesce(f.regexp_extract('duration', DURATION_PATTERN, 1).cast('int'), f.lit(0)) * 3600
+    + f.coalesce(f.regexp_extract('duration', DURATION_PATTERN, 2).cast('int'), f.lit(0)) * 60
+    + f.coalesce(f.regexp_extract('duration', DURATION_PATTERN, 3).cast('int'), f.lit(0))
+)
+
 local_bronze_videos = slicer.run()
 load_videos = local_bronze_videos.select(
     f.col('id').alias('video_id'),
     f.col('title').alias('title'),
     f.col('description').alias('description'),
     f.col('categoryid').alias('category_id'),
-    f.col('livebroadcastcontent').alias('is_stream'),
+    f.col('livebroadcastcontent').alias('live_broadcast_content'),
     f.col('defaultlanguage').alias('default_language'),
-    f.col('duration').alias('duration'),
+    duration_sec.alias('duration_sec'),
     f.col('dimension').alias('dimension'),
     f.col('definition').alias('definition'),
-    f.col('caption').alias('caption'),
-    f.col('licensedcontent').alias('license'),
+    f.col('caption').cast('boolean').alias('caption'),
+    f.col('licensedcontent').cast('boolean').alias('license'),
     f.col('contentrating').alias('content_rating'),
     f.col('projection').alias('projection'),
     f.col('calendar_dt').cast('timestamp').alias('valid_from_dttm')
