fd54551cf1cbd40aeb631734ecb507aa022ba6e8
lsnginterests <lsnginterests@gmail.com>
2026-08-07 16:42:39 +0300

    cast compare columns to string in collapse

--- dataplatform/etl_tools/collapse.py
+++ dataplatform/etl_tools/collapse.py
@@ -5,7 +5,7 @@ def collapse(dataframe: DataFrame, key_columns: list, compare_columns: list) -> 
     if 'valid_from_dttm' not in dataframe.columns:
                 raise ValueError(f'dataframe {dataframe} must have valid_from_dttm column')
 
-    columns = [f.coalesce(f.col(col), f.lit('none')) for col in compare_columns if col != 'valid_from_dttm' and col not in key_columns]
+    columns = [f.coalesce(f.col(col).cast('string'), f.lit('none')) for col in compare_columns if col != 'valid_from_dttm' and col not in key_columns]
     w = Window.partitionBy(*key_columns).orderBy(f.col('valid_from_dttm').asc())
     df = dataframe.withColumn('hashkey', f.md5(f.concat_ws('||', *columns))) \
         .withColumn('lag_hashkey', f.lag(f.col('hashkey')).over(w)) \
