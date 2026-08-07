c09131d2a85a921bcd3c333098f676216f53827d
lsnginterests <lsnginterests@gmail.com>
2026-08-07 10:16:56 +0300

    add gold calendar dimension pipeline

---
+++ dataplatform/pipelines/gold/load_gold_dim_calendar.py
@@ -0,0 +1,45 @@
+from dataplatform.setting.spark_session import get_spark
+from dataplatform.etl_tools import SCD1Loader
+
+from pyspark.sql import functions as f
+
+
+spark = get_spark()
+
+spark.sql('''
+    create table if not exists local.gold.dim_calendar (
+        calendar_dt date,
+        monthday int,
+        month int,
+        quarter int,
+        year int,
+        week_of_year int,
+        iso_year int,
+        weekday int,
+        is_weekend boolean,
+        processed_dttm timestamp
+    ) using iceberg
+''')
+
+calendar = spark.sql("select explode(sequence(date '2015-01-01', date '2035-01-01', interval 1 day)) as calendar_dt")
+to_load = calendar.select(
+    f.col('calendar_dt'),
+    (f.extract(f.lit('day'), 'calendar_dt').cast('int').alias('monthday')),
+    f.month('calendar_dt').alias('month'),
+    f.quarter('calendar_dt').alias('quarter'),
+    f.year('calendar_dt').alias('year'),
+    f.weekofyear('calendar_dt').alias('week_of_year'),
+    f.extract(f.lit('yearofweek'), 'calendar_dt').alias('iso_year'),
+    (f.weekday('calendar_dt') + 1).alias('weekday')
+).withColumn('is_weekend', f.col('weekday') > 5).alias('is_weekend')
+
+if not to_load.isEmpty():
+    loader = SCD1Loader(
+        input=to_load,
+        output='local.gold.dim_calendar',
+        key_columns=['calendar_dt'],
+        mode='upsert'
+    )
+    loader.run()
+
+spark.stop()
